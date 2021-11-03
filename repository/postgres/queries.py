575c2feefdac378939828b424135d70d9efb56ce
Vu Tran <vutran@Vus-iMac.local>
2021-11-03 22:53:39 +0700

    Fix queries, alter data type to timestamp with timezone

--- repository/postgres/queries.py
+++ repository/postgres/queries.py
@@ -37,7 +37,24 @@ VALUES ($1, $2, $3, $4)
 RETURNING *
 """
 
-FIND_IMAGE_BY_ID = "SELECT * FROM images WHERE id = $1"
+FIND_IMAGE_BY_ID = """
+WITH img AS (
+    SELECT * FROM images WHERE id = $1
+),
+tag_ids AS (
+    SELECT *
+    FROM tagged
+    WHERE image = $1
+),
+tag_names AS (
+    SELECT string_agg(name, ',') AS tags
+    FROM tags
+    WHERE id IN (SELECT tag FROM tag_ids)
+)
+SELECT *
+FROM img
+LEFT JOIN tag_names ON TRUE;
+"""
 
 UPSERT_TAGS = """
 WITH items (name) AS (SELECT r.name FROM unnest($1::tags[]) as r),
@@ -80,10 +97,49 @@ image_ids AS (
         SELECT image
         FROM tagged
         WHERE tag in (SELECT id FROM tag_items)
-        AND created_at >= $4 AND created_at <= $5
+        AND created_at >= $3 AND created_at <= $4
+        GROUP BY image, created_at
         ORDER BY created_at DESC
         LIMIT $2
-        OFFSET $3
+),
+image_tag_ids AS (
+        SELECT image, tag
+        FROM tagged
+        WHERE image in (SELECT image FROM image_ids)
+),
+image_tags AS (
+        SELECT image, string_agg(tags.name, ',') as tags
+        FROM image_tag_ids
+        LEFT JOIN tags
+        ON image_tag_ids.tag = tags.id
+        GROUP BY image
+),
+image_tags_full_info AS (
+
+SELECT images.*, image_tags.tags
+        FROM image_tags
+        LEFT JOIN images
+        ON image_tags.image = images.id
+        ORDER BY images.created_at DESC
+)
+SELECT * FROM image_tags_full_info
+"""
+
+SEARCH_TAGGED_IMAGES_WITH_PAGE = """
+WITH tag_items AS (
+        SELECT id, name
+        FROM tags
+        WHERE name IN (SELECT r.name FROM unnest($1::tags[]) as r)
+),
+image_ids AS (
+        SELECT image
+        FROM tagged
+        WHERE tag in (SELECT id FROM tag_items)
+        AND (created_at, image) >= ($3, $5)
+        AND (created_at, image) < ($4, $5)
+        GROUP BY image, created_at
+        ORDER BY created_at DESC
+        LIMIT $2
 ),
 image_tag_ids AS (
         SELECT image, tag
