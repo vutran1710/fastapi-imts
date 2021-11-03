af17d005c0910cbc4c7c2cce4b235198c0925212
Vu Tran <vutran@Vus-iMac.local>
2021-11-03 23:19:52 +0700

    Set TIMEZONE for app, apply flake on test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from logzero import logger
 
 from settings import settings
@@ -6,3 +8,4 @@ logger.info("Setup env for Testing ==========================")
 
 settings.STAGE = "test"
 settings.PG_DATABASE = "testdb"
+environ["TZ"] = "Asia/Ho_Chi_Minh"
